1bc9c6c7bc1733989a63a89a22896911517ae56f
Kaitlin Newson <kaitlin.newson@utoronto.ca>
2021-11-29 09:49:55 -0500

    zoom map to added features

--- project/public/js/map.js
+++ project/public/js/map.js
@@ -1,7 +1,6 @@
 // Get file from Dataverse API
 
 // initialize the map
-// TODO - set view based on map features
 var map = L.map('map').fitWorld();
 map.setZoom(1.25);
 
@@ -10,9 +9,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// setup form listener
 const mapForm = document.getElementById('mapForm');
 mapForm.addEventListener('submit', loadMap);
 
+// load map based on form entry
 function loadMap(event){
     event.preventDefault();
     const dataverseUrl = mapForm.elements["dataverse"].value;
@@ -20,12 +21,13 @@ function loadMap(event){
     const idType = mapForm.elements["idType"].value;
     const apiKey = mapForm.elements["apiKey"].value;
 
-    fetch("/getFile?dataverse="+dataverseUrl+"&fileId="+fileId+"&idType="+idType+"&apiKey="+apiKey)
+    fetch("/getFile?dataverse=" + dataverseUrl + "&fileId=" + fileId + "&idType=" + idType + "&apiKey=" + apiKey)
     .then(function(response) {
         return response.json();
     })
     .then(function (mapJSON) {
         // TODO: clear previous map layers
-        L.geoJSON(mapJSON).addTo(map);
+        let geoJson = L.geoJSON(mapJSON).addTo(map);
+        map.fitBounds(geoJson.getBounds());
     }).catch(console.error);
 }
